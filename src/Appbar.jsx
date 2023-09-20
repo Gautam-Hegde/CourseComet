7467948ca209dd76e7a94662d13b03ed044fb86f
Gautam-Hegde <gautamatjob@protonmail.com>
2023-09-20 23:20:20 +0530

    chore: user self fetch

--- src/Appbar.jsx
+++ src/Appbar.jsx
@@ -1,8 +1,28 @@
 import { Button, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Appbar() {
   const navigate = useNavigate();
+  const[userEmail,setUserEmail]=useState(null);
+
+  useEffect(()=>{
+    function callback2(data){
+      //console.log(data);
+      if(data.username){
+        setUserEmail(data.username);
+      }
+    }
+    function callbackfn(res){
+      res.json().then(callback2)
+    }
+    fetch("http://localhost:3000/admin/me",{
+      method:"GET",
+      headers:{
+        "Authorization":"Bearer " + localStorage.getItem("token")
+      }
+    }).then(callbackfn)
+  },[])
   return (
     <div
       style={{
