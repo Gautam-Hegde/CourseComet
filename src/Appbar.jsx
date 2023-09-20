b586f5b565b4d8f50ecb2ff3d0a2fe4f3388892e
Gautam-Hegde <gautamatjob@protonmail.com>
2023-09-20 23:42:06 +0530

    chore: conditional render

--- src/Appbar.jsx
+++ src/Appbar.jsx
@@ -4,25 +4,31 @@ import { useNavigate } from "react-router-dom";
 
 function Appbar() {
   const navigate = useNavigate();
-  const[userEmail,setUserEmail]=useState(null);
+  const [userEmail, setUserEmail] = useState(null);
 
-  useEffect(()=>{
-    function callback2(data){
-      //console.log(data);
-      if(data.username){
+  useEffect(() => {
+    function callback2(data) {
+      if (data.username) {
         setUserEmail(data.username);
       }
     }
-    function callbackfn(res){
-      res.json().then(callback2)
+    function callbackfn(res) {
+      res.json().then(callback2);
     }
-    fetch("http://localhost:3000/admin/me",{
-      method:"GET",
-      headers:{
-        "Authorization":"Bearer " + localStorage.getItem("token")
-      }
-    }).then(callbackfn)
-  },[])
+    fetch("http://localhost:3000/admin/me", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+    }).then(callbackfn);
+  }, []);
+
+  const handleLogout = () => {
+    // Perform logout action here, e.g., clear localStorage, etc.
+    localStorage.removeItem("token"); // Assuming you store the token in localStorage
+    setUserEmail(null); // Clear the userEmail state
+  };
+
   return (
     <div
       style={{
@@ -37,23 +43,47 @@ function Appbar() {
 
       <div style={{ display: "flex" }}>
         <div style={{ marginRight: 10 }}>
-          <Button style={{ backgroundColor: "#eb4d4b" }} variant="contained"
-           onClick={()=>{
-            navigate("/signup");
-          }}
-          >
-            SignUp
-          </Button>
-        </div>
-        <div style={{ marginRight: 10 }}>
-          <Button variant="contained"
-          onClick={()=>{
-            navigate("/signin");
-          }}
-          >SignIn</Button>
+          {userEmail && (
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <Typography variant="body1" style={{ marginRight: 10 }}>
+                {userEmail}
+              </Typography>
+              <Button
+                style={{ backgroundColor: "#eb4d4b" }}
+                variant="contained"
+                onClick={handleLogout}
+              >
+                Logout
+              </Button>
+            </div>
+          )}
+
+          {!userEmail && (
+            <div>
+              <Button
+                style={{ backgroundColor: "#eb4d4b" }}
+                variant="contained"
+                onClick={() => {
+                  navigate("/signup");
+                }}
+              >
+                SignUp
+              </Button>
+              <Button
+                style={{ backgroundColor: "#eb4d4b", marginLeft: 10 }}
+                variant="contained"
+                onClick={() => {
+                  navigate("/signin");
+                }}
+              >
+                SignIn
+              </Button>
+            </div>
+          )}
         </div>
       </div>
     </div>
   );
 }
+
 export default Appbar;
